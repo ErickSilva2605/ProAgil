c003cfddda6c48b2062773d8e176990674f2eadb
Erick Augusto <erick.dev.it@gmail.com>
2019-11-02 19:17:54 -0300

    Implementando filtro no grid de eventos

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,11 +8,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
+  // Propriedades
+  FiltroLista: string;
+  get filtroLista(): string {
+    return this.FiltroLista;
+  }
+  set filtroLista(value: string) {
+    this.FiltroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  // Variaveis
   eventos: any = [];
+  eventosFiltrados: any = [];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
-  filtroLista = '';
 
   constructor(private http: HttpClient) { }
 
@@ -24,12 +35,22 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      e => e.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      e.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      e.data.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      e.lote.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   getEventos() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.eventos = response;
-    }, error => {
-      console.log(error);
-    });
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
