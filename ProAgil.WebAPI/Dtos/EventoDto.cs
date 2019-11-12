df01198f01434abc83579a19a9dc5e9e5f53acf1
Erick Augusto <erick.dev.it@gmail.com>
2019-11-11 22:55:20 -0300

    Implementando Data Annotations utilizando os decorators

--- ProAgil.WebAPI/Dtos/EventoDto.cs
+++ ProAgil.WebAPI/Dtos/EventoDto.cs
@@ -1,16 +1,28 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProAgil.WebAPI.Dtos
 {
     public class EventoDto
     {
         public int Id { get; set; }
+        
+        [Required (ErrorMessage="Campo Obrigatório.")]
+        [StringLength(100, MinimumLength=3, ErrorMessage="Local é entre 3 e 100 caracteres.")]
         public string Local { get; set; }
         public string Data { get; set; }
+
+        [Required(ErrorMessage="O Tema deve ser preenchido.")]
         public string Tema { get; set; }
+
+        [Range(2, 120000, ErrorMessage="Quantidade de Pessoas é entre 2 e 120000.")]
         public int QtdPessoas { get; set; }
         public string ImagemUrl { get; set; }
+
+        [Phone(ErrorMessage ="O valor informado no campo {0} é invalido")]
         public string Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage ="Informe um e-mail valido")]
         public string Email { get; set; }
         public List<LoteDto> Lotes { get; set; }
         public List<RedeSocialDto> RedeSociais { get; set; }
--- ProAgil.WebAPI/Dtos/LoteDto.cs
+++ ProAgil.WebAPI/Dtos/LoteDto.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProAgil.WebAPI.Dtos
 {
     public class LoteDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage="O Nome deve ser preenchido.")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage="O preço deve ser preenchido.")]
         public double Preco { get; set; }
         public string DataInicio { get; set; }
         public string DataFim { get; set; }
+
+        [Range(1, 120000, ErrorMessage="Quantidade deve ser entre 1 e 120000.")]
         public int Quantidade { get; set; }
     }
 }
